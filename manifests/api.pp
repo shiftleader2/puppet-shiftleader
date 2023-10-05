52fd46e9402bb53bb7a23c9498c84dd67d3307af
Eigil Obrestad <eigil-git@obrestad.org>
2023-10-05 15:48:05 +0200

    Add vhost for puppet-reports

--- manifests/api.pp
+++ manifests/api.pp
@@ -3,6 +3,7 @@ class shiftleader::api (
   String                     $api_name       = $shiftleader::params::api_name,
   Variant[String, Boolean]   $api_cert       = $shiftleader::params::api_cert,
   Variant[String, Boolean]   $api_key        = $shiftleader::params::api_key,
+  String                     $puppetapi_name = $shiftleader::params::puppetapi_name,
   Enum['running', 'stopped'] $service_ensure = $shiftleader::params::service_ensure, 
   Boolean                    $service_enable = $shiftleader::params::service_enable,
 ) inherits shiftleader::params {
@@ -18,5 +19,10 @@ class shiftleader::api (
   ::shiftleader::api::vhost { $api_name:
     cert => $api_cert, 
     key  => $api_key,
+  }
+
+  ::shiftleader::api::vhost { $puppetapi_name:
+    cert => "/etc/puppetlabs/puppet/ssl/certs/${::fqdn}.pem", 
+    key  => "/etc/puppetlabs/puppet/ssl/private_keys/${::fqdn}.pem",
   }
 }
