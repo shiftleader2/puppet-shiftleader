3b1195082463244f1e5b5f1af6b14a75b212eb8c
Eigil Obrestad <eigil-git@obrestad.org>
2023-11-07 14:05:09 +0100

    Make the processes-parameter being overridable

--- manifests/api.pp
+++ manifests/api.pp
@@ -3,6 +3,7 @@ class shiftleader::api (
   String                     $api_name       = $shiftleader::params::api_name,
   Variant[String, Boolean]   $api_cert       = $shiftleader::params::api_cert,
   Variant[String, Boolean]   $api_key        = $shiftleader::params::api_key,
+  Integer                    $processes      = $::facts['processors']['count'],
   String                     $puppetapi_name = $shiftleader::params::puppetapi_name,
   Enum['running', 'stopped'] $service_ensure = $shiftleader::params::service_ensure, 
   Boolean                    $service_enable = $shiftleader::params::service_enable,
@@ -17,12 +18,14 @@ class shiftleader::api (
   }
   
   ::shiftleader::api::vhost { $api_name:
-    cert => $api_cert, 
-    key  => $api_key,
+    cert      => $api_cert, 
+    key       => $api_key,
+    processes => $processes,
   }
 
   ::shiftleader::api::vhost { $puppetapi_name:
-    cert => "/etc/puppetlabs/puppet/ssl/certs/${::fqdn}.pem", 
-    key  => "/etc/puppetlabs/puppet/ssl/private_keys/${::fqdn}.pem",
+    cert      => "/etc/puppetlabs/puppet/ssl/certs/${::fqdn}.pem", 
+    key       => "/etc/puppetlabs/puppet/ssl/private_keys/${::fqdn}.pem",
+    processes => $processes,
   }
 }
