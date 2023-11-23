d1e655d7972e57315b203125bd02a4b7e8f9c1cd
Eigil Obrestad <eigil-git@obrestad.org>
2023-11-23 11:08:49 +0100

    Allow the clients to use https even if the api-servers dont terminate the TLS-traffic themselves

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,18 +15,19 @@ class shiftleader::params (
   String                     $database_allowed_hosts = '127.0.0.1',
   String                     $database_name = 'shiftleader2',
   String                     $database_username = 'shiftleader2',
+  Boolean                    $external_tls = false,
   Enum['running', 'stopped'] $service_ensure = 'running',
   Boolean                    $service_enable = true,
   Variant[String, Boolean]   $web_cert = false,
   Variant[String, Boolean]   $web_key = false,
 ){
-  if($api_cert) {
+  if($api_cert or $external_tls) {
     $api_url = "https://${api_name}"
   } else {
     $api_url = "http://${api_name}"
   }
 
-  if($web_cert) {
+  if($web_cert or $external_tls) {
     $web_url = "https://${web_name}"
   } else {
     $web_url = "http://${web_name}"
