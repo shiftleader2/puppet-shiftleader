a9cd5b42deb94680a296a613e1331090c84705fd
Eigil Obrestad <eigil-git@obrestad.org>
2024-01-19 14:12:42 +0100

    Make the log-format being overridable

--- manifests/api.pp
+++ manifests/api.pp
@@ -1,33 +1,36 @@
 # Installs and configures the shiftleader2 API 
 class shiftleader::api (
-  String                     $api_name       = $shiftleader::params::api_name,
-  Variant[String, Boolean]   $api_cert       = $shiftleader::params::api_cert,
-  Variant[String, Boolean]   $api_key        = $shiftleader::params::api_key,
-  Integer                    $processes      = $::facts['processors']['count'],
-  String                     $puppetapi_name = $shiftleader::params::puppetapi_name,
-  Variant[String, Boolean]   $puppetapi_cert = "/etc/puppetlabs/puppet/ssl/certs/${::fqdn}.pem",
-  Variant[String, Boolean]   $puppetapi_key  = "/etc/puppetlabs/puppet/ssl/private_keys/${::fqdn}.pem", 
-  Enum['running', 'stopped'] $service_ensure = $shiftleader::params::service_ensure, 
-  Boolean                    $service_enable = $shiftleader::params::service_enable,
+  Optional[String]           $access_log_format = undef,
+  String                     $api_name          = $shiftleader::params::api_name,
+  Variant[String, Boolean]   $api_cert          = $shiftleader::params::api_cert,
+  Variant[String, Boolean]   $api_key           = $shiftleader::params::api_key,
+  Integer                    $processes         = $::facts['processors']['count'],
+  String                     $puppetapi_name    = $shiftleader::params::puppetapi_name,
+  Variant[String, Boolean]   $puppetapi_cert    = "/etc/puppetlabs/puppet/ssl/certs/${::fqdn}.pem",
+  Variant[String, Boolean]   $puppetapi_key     = "/etc/puppetlabs/puppet/ssl/private_keys/${::fqdn}.pem",
+  Enum['running', 'stopped'] $service_ensure    = $shiftleader::params::service_ensure,
+  Boolean                    $service_enable    = $shiftleader::params::service_enable,
 ) inherits shiftleader::params {
   include ::shiftleader::apt
   include ::shiftleader::deps
   include ::shiftleader::api::config
-  
+
   package { 'shiftleader2-api':
     ensure => 'present',
     tag    => 'shiftleader-package',
   }
-  
+
   ::shiftleader::api::vhost { $api_name:
-    cert      => $api_cert, 
-    key       => $api_key,
-    processes => $processes,
+    access_log_format => $access_log_format,
+    cert              => $api_cert,
+    key               => $api_key,
+    processes         => $processes,
   }
 
   ::shiftleader::api::vhost { $puppetapi_name:
-    cert      => $puppetapi_cert,
-    key       => $puppetapi_key,
-    processes => $processes,
+    access_log_format => $access_log_format,
+    cert              => $puppetapi_cert,
+    key               => $puppetapi_key,
+    processes         => $processes,
   }
 }
--- manifests/web.pp
+++ manifests/web.pp
@@ -1,22 +1,24 @@
 # Installs and configures the shiftleader2 Web-interface 
 class shiftleader::web (
-  Enum['running', 'stopped'] $service_ensure = $shiftleader::params::service_ensure, 
-  Boolean                    $service_enable = $shiftleader::params::service_enable,
-  String                     $web_name       = $shiftleader::params::web_name,
-  Variant[String, Boolean]   $web_cert       = $shiftleader::params::web_cert,
-  Variant[String, Boolean]   $web_key        = $shiftleader::params::web_key,
+  Optional[String]           $access_log_format = undef,
+  Enum['running', 'stopped'] $service_ensure    = $shiftleader::params::service_ensure,
+  Boolean                    $service_enable    = $shiftleader::params::service_enable,
+  String                     $web_name          = $shiftleader::params::web_name,
+  Variant[String, Boolean]   $web_cert          = $shiftleader::params::web_cert,
+  Variant[String, Boolean]   $web_key           = $shiftleader::params::web_key,
 ) inherits shiftleader::params {
   include ::shiftleader::apt
   include ::shiftleader::deps
   include ::shiftleader::web::config
-  
+
   package { 'shiftleader2-web':
     ensure => 'present',
     tag    => 'shiftleader-package',
   }
-  
+
   ::shiftleader::web::vhost { $web_name:
-    cert => $web_cert, 
-    key  => $web_key,
+    access_log_format => $access_log_format,
+    cert              => $web_cert,
+    key               => $web_key,
   }
 }
