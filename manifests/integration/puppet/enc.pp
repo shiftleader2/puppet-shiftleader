f0bc7503bbe1a5da216da16508b22c4a875f4e2a
Eigil Obrestad <eigil-git@obrestad.org>
2026-03-03 15:34:53 +0100

    Actually setting the setting is smart.

--- manifests/integration/puppet/enc.pp
+++ manifests/integration/puppet/enc.pp
@@ -1,9 +1,9 @@
-#Configures the puppetmaster to use the ShiftLeader2 ENC.
+# Configures the puppetmaster to use the ShiftLeader2 ENC.
 class shiftleader::integration::puppet::enc {
   include ::shiftleader::worker::puppet
 
   puppet::config::server {
     'node_terminus':  value => 'exec';
-    'external_nodes': value => 'external_nodes';
+    'external_nodes': value => '/usr/local/bin/shiftleader2-puppet-enc';
   }
 }
